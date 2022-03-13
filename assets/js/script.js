639197fdbf8abd344a3cf980649807dfdd92a353
Khyan Alvarez <khyanalvarez@gmail.com>
2022-03-13 15:39:52 -0400

    added vars for password criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,27 @@
 // Assignment code here
+function generatePassword() {
 
+  //password length prompt
+  var passwordLength = parseInt(window.prompt("Please choose a length between 8 and 128.", ""), 10);
+  while(passwordLength < 8 || 128 < passwordLength || isNaN(passwordLength)) {
+    var passwordLength = parseInt(window.prompt("Please choose a length between 8 and 128.", ""), 10);
+  }
+
+  // password prompts
+  var upperConfirm = window.confirm("Would you like to use uppercase letters?")
+  var lowerConfirm = window.confirm("Would you like to use lowercase letters?")
+  var numberConfirm = window.confirm("would you like to use numbers?")
+  var specialCharacters = window.confirm("Would you like to use special characters?")
+
+  //character var
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  var numbers = "1234567890"
+  var specialCharacters = "\"\'\@\\!#$%^&()|{}[]^*_+-=<>?"
+
+  //generator statements
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
