e9f8cb89656f74f0ad88f0dd64cc6bb253a4e22e
Khyan Alvarez <khyanalvarez@gmail.com>
2022-03-13 17:32:11 -0400

    added if and else if statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,16 +11,102 @@ function generatePassword() {
   var upperConfirm = window.confirm("Would you like to use uppercase letters?")
   var lowerConfirm = window.confirm("Would you like to use lowercase letters?")
   var numberConfirm = window.confirm("would you like to use numbers?")
-  var specialCharacters = window.confirm("Would you like to use special characters?")
+  var specialConfirm = window.confirm("Would you like to use special characters?")
 
   //character var
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   var lowerCase = "abcdefghijklmnopqrstuvwxyz"
   var numbers = "1234567890"
-  var specialCharacters = "\"\'\@\\!#$%^&()|{}[]^*_+-=<>?"
+  var specialChar = "\"\'\@\\!#$%^&()|{}[]^*_+-=<>?"
 
   //generator statements
+  if (upperConfirm && lowerConfirm && numberConfirm && specialConfirm) {
+    var randomChar = function() {
+      var character = (upperCase + lowerCase + numbers + specialChar)[Math.floor(Math.random() * (upperCase.length + lowerCase.length + numbers.length + specialChar.length))]
+      return character
+    }
+  } else if (upperConfirm && lowerConfirm && numberConfirm) {
+    var randomChar = function() {
+      var character = (upperCase + lowerCase + numbers)[Math.floor(Math.random() * (upperCase.length + lowerCase.length + numbers.length))]
+      return character
+    }
+  } else if (upperConfirm && lowerConfirm && specialConfirm) {
+    var randomChar = function() {
+      var character = (upperCase + lowerCase + specialChar)[Math.floor(Math.random() * (upperCase.length + lowerCase.length + specialChar.length))]
+      return character
+    }
+  } else if (lowerConfirm && numbers && specialConfirm) {
+    var randomChar = function() {
+      var character = (lowerCase + numbers + specialChar)[Math.floor(Math.random() * (lowerCase.length + numbers.length + specialChar.length))]
+      return character
+    }
+  }else if (upperConfirm + numberConfirm + specialConfirm) {
+    var randomChar = function() {
+      var character = (upperCase + numbers + specialChar)[Math.floor(Math.random() * (upperCase.length + numbers.length + specialChar))]
+      return character
+    }
+  }else if (upperConfirm + lowerConfirm) {
+    var randomChar = function() {
+      var randomChar = (upperCase + lowerCase)[Math.floor(Math.random() * (upperCase.length + lowerCase.length))]
+      return character
+    }
+  } else if (upperConfirm + specialConfirm) {
+    var randomChar = function() {
+      var randomChar = (upperCase + specialChar)[Math.floor(Math.random() * (upperCase.length + specialChar.length))]
+      return character
+    }
+  } else if (upperConfirm + numberConfirm) {
+    var randomChar = function() {
+      var randomChar = (upperCase + numbers)[Math.floor(Math.random() * (upperCase.length + numbers.length))]
+      return character
+    }
+  } else if (lowerConfirm + specialConfirm) {
+    var randomChar = function() {
+      var randomChar = (lowerCase + specialChar)[Math.floor(Math.random() * (lowerCase.length + specialChar.length))]
+      return character 
+    }
+  } else if (lowerConfirm + numberConfirm) {
+    var randomChar = function() {
+      var randomChar = (lowerCase + numbers)[Math.floor(Math.random() * (lowerCase.length + numbers.length))]
+      return character
+    }
+  } else if (numberConfirm + specialConfirm) {
+    var randomChar = function() {
+      var randomChar = (numbers + specialChar)[Math.floor(Math.random() * (numbers.length + specialChar.length))]
+      return character
+    }
+  } else if (upperConfirm) {
+    var randomChar = function() {
+      var randomChar = (upperCase)[Math.floor(Math.random() * (upperCase.length))]
+      return character
+    }
+  } else if (lowerConfirm) {
+    var randomChar = function() {
+      var randomChar = (lowerCase)[Math.floor(Math.random() * (lowerCase.length))]
+      return character
+    }
+  } else if (specialConfirm) {
+    var randomChar = function() {
+      var randomChar = (specialChar)[Math.floor(Math.random() * (specialChar.length))]
+      return character
+    }
+  } else if (numberConfirm) {
+    var randomChar = function() {
+      var randomChar = (numbers)[Math.floor(Math.random() * (numbers.length))]
+      return character
+    }
+  }
 
+  if (randomChar === undefined) {
+    var randomPassword = "You need to select at least one character type!"
+  }
+  else {
+    var randomPassword = ""
+    for (var i = 0; i < passwordLength; i++) {
+      randomPassword += randomChar();
+    }
+  }
+  return randomPassword
 }
 
 // Get references to the #generate element
